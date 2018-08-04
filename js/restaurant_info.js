301a105565b30ea52fd344d0c9cde4a69cb1ddb1
vsskanand <anand_fun30@yahoo.com>
2018-08-04 17:22:32 -0400

    mapbox api key to restaurant_info.js

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -1,5 +1,7 @@
 let restaurant;
 var newMap;
+var mapboxApiKey = 'pk.eyJ1IjoiYW5hbmR2IiwiYSI6ImNqamQxM3g3dTQ4dmQza2xmNnBiZDdsc2sifQ.xCzDiVKV6CGvBfuv1KBLdw';
+
 
 /**
  * Initialize map as soon as the page is loaded.
@@ -22,7 +24,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: mapboxApiKey,
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
