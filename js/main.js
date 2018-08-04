ba0d55c3108cc055a110344e499f67c74bb2fc14
vsskanand <anand_fun30@yahoo.com>
2018-08-04 16:03:32 -0400

    Getting Started....
    1. added mapboxToken
    2. cleaned up HMTL code using HTML/CSS/JS prettify
    3. Fixed nav title padding issue

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
-let restaurants,
-  neighborhoods,
-  cuisines
+let restaurants, neighborhoods, cuisines
 var newMap
 var markers = []
-var mapboxApiKey = 'getting started !';
+var mapboxApiKey = 'pk.eyJ1IjoiYW5hbmR2IiwiYSI6ImNqamQxM3g3dTQ4dmQza2xmNnBiZDdsc2sifQ.xCzDiVKV6CGvBfuv1KBLdw';
 
 /**
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
@@ -79,7 +77,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: mapboxApiKey,
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
